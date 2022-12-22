e4af2bb144d9a83841004f8d35842a0d9c0cb46f
saurabhje <work.saurabh@outlook.com>
2022-12-22 17:32:12 +0000

    m

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const title = document.getElementById('title').value
-const author = document.getElementById('author').value;
-const pages = document.getElementById('pages').value;
-const read = document.getElementById('read').value;
-
 
 const addbtn = document.getElementById('addbtn');
+addbtn.addEventListener('click',()=>{
+    form.style.display ="block";
+})
+
 const subbtn = document.getElementById('subbtn');
+subbtn.addEventListener('click',addBooktoLibrary);
+
 const form = document.getElementById('form')
 const showbtn = document.getElementById('showbook')
 
-function handleForm(event) { 
-    event.preventDefault(); 
-    } 
-form.addEventListener('submit',handleForm);
 
 const library = [];
 
@@ -22,20 +19,25 @@ function book(title,author,pages,read){
     this.pages = pages;
     this.read = read;
 }
-addbtn.addEventListener('click',()=>{
-    form.style.display ="block";
-})
-
-let newbook = new book(title,author,pages,read);
-
-subbtn.addEventListener('click',()=>{
+let newbook;
+function addBooktoLibrary(event){
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').value;
+    event.preventDefault();
+    newbook = new book(title,author,pages,read);
     library.push(newbook);
-})
-console.log(newbook.title)
+}
 
 function showbook(){
-document.getElementsByClassName('title').innerHTML = newbook.title;
-console.log(newbook.title);
+const libdiv = document.querySelector('.bookdetails');
+const bookdiv = document.createElement('div');
+bookdiv.setAttribute("class","title");
+bookdiv.textContent = newbook.title;
+libdiv.appendChild(bookdiv);
+console.log(newbook.title)
+
 }
 showbtn.addEventListener('click',showbook);
 
